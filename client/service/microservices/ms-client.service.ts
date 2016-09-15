1540923d6d144ce3cbc2c827d5c6cf5b17f0b269
mos <myownsumm@gmail.com>
2016-09-15 10:53:36 +0300

    makeMsCall added

--- client/service/microservices/ms-client.service.ts
+++ client/service/microservices/ms-client.service.ts
@@ -10,20 +10,45 @@ export class MsClientService {
     secureStr: string = 'someRandomGeneratedString123';
     callerMsName: string = 'app-ui';
 
-    protected Http: Http;
-
     constructor(protected Http: Http) {
         this.Http = Http;
     }
 
     protected handleError(error: any): Promise<any> {
-        console.error('An error occurred', error); // for demo purposes only
+        console.error('An error occurred', error);
         return Promise.reject(error.message || error);
     }
 
-    public makeApiCall(): {} {
-        return {
-            property: 'value'
+    /**
+     * Make http call to microservice.
+     *
+     * @todo implement POST, PUT, DELETE
+     *
+     * @param {String} action
+     * @param {String} method GET, POST, PUT, DELETE
+     * @param {} data
+     * @return {Promise<{}>}
+     */
+    public makeMsCall(action: string, method: string, data: {} = {}): Promise<[{}]> {
+        let params;
+
+        if (method === 'GET') {
+            params = new URLSearchParams();
+            params.set('params', JSON.stringify(data));
         }
+
+        return this.Http.get(this.url + action, {search: params})
+            .toPromise()
+            .then(function (response) {
+                let resObj = response.json();
+
+                if (!resObj.success) {
+                    throw new Error(resObj.message);
+                    // todo show errors from resObj.errors
+                }
+
+                return resObj.result;
+            })
+            .catch(this.handleError);
     }
 }
--- client/service/microservices/ms-user-client.service.ts
+++ client/service/microservices/ms-user-client.service.ts
@@ -4,11 +4,8 @@ import {Headers, Http, URLSearchParams} from '@angular/http';
 export class MsUserClientService extends MsClientService {
     url = 'http://localhost:3002';
 
-    getMe(): Promise<[{}]> {
-        return this.Http.get(this.url + '/me')
-            .toPromise()
-            .then(response => response.json().result)
-            .catch(this.handleError);
+    getMe() {
+        return this.makeMsCall('/me', 'GET');
     }
 
 }
