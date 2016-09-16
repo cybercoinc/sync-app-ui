c2b4031cd443111c482c7f6b4ca97f0e7ef1c37b
mos <myownsumm@gmail.com>
2016-09-16 14:34:48 +0300

    content html for tabs

--- client/modules/projects/project-wizard/project-wizard.component.ts
+++ client/modules/projects/project-wizard/project-wizard.component.ts
@@ -11,27 +11,35 @@ export class ProjectWizardComponent {
     tabs = [
         {
             label: 'Step 1 - Choose Procore project',
-            content: 'procore projects dropdown'
+            content: `
+                <a href="#">Check it</a>
+            `,
+
+            disabled: false,
         },
 
         {
             label: 'Step 2 - Choose Smartsheet project',
-            content: 'procore projects dropdown or NO button'
+            content: 'procore projects dropdown or NO button',
+            disabled: true,
         },
 
         {
             label: 'Step 3 - Match Smartsheet column heading',
-            content: 'Smartsheet columns dropdowns to column names'
+            content: 'Smartsheet columns dropdowns to column names',
+            disabled: true,
         },
 
         {
             label: 'Step 4 - Set billing project name and PRB person',
-            content: 'Custom Billing Project Name/code input, PBR person dropdown, Issue separate invoice checkbox'
+            content: 'Custom Billing Project Name/code input, PBR person dropdown, Issue separate invoice checkbox',
+            disabled: true,
         },
 
         {
             label: 'Step 5 - Final step',
-            content: 'Project status dropdown, task titles include dropdown, private tasks and todos dropdown, working days checkbox, non working days textarea'
+            content: 'Project status dropdown, task titles include dropdown, private tasks and todos dropdown, working days checkbox, non working days textarea',
+            disabled: true,
         },
     ];
 }
