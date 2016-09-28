4bc2f8e98c9ce68611409efbe99d60b58b884161
Anton Vovchenko <antonvovchenko@gmail.com>
2016-09-28 14:52:56 +0300

    update app styles

--- client/app.component.ts
+++ client/app.component.ts
@@ -5,6 +5,7 @@ import {MdIconRegistry} from '@angular2-material/icon';
 @Component({
     selector: 'app',
     templateUrl: 'client/app.component.html',
+    styleUrls: ['client/app.component.css'],
     viewProviders: [MdIconRegistry],
     encapsulation: ViewEncapsulation.None,
 })
