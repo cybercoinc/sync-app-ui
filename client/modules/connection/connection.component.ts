e6a4db637f689c3693036b6beab09d80b91273f7
mos <myownsumm@gmail.com>
2016-09-28 16:02:48 +0300

    connection component

--- client/modules/connection/connection.component.ts
+++ client/modules/connection/connection.component.ts
@@ -1,9 +1,21 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {MsUserClientService} from '../../service/microservices/ms-user-client.service'
 
 @Component({
     selector: "connection",
-    templateUrl: `client/modules/connection/connection.component.html`
+    templateUrl: `client/modules/connection/connection.component.html`,
+    styleUrls: ['client/modules/connection/connection.component.css']
 })
-export class ConnectionComponent {
+export class ConnectionComponent implements OnInit {
 
+    me: {};
+
+    constructor(protected MsUserClientService: MsUserClientService) {
+
+    }
+
+    ngOnInit() {
+        this.MsUserClientService.getMe()
+            .then(me => this.me = me);
+    }
 }
