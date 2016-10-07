c8aca0fb7cd4ddc2cb8d87cb799ebbfad52bd64c
mos <myownsumm@gmail.com>
2016-10-07 14:27:26 +0300

    user email in menu

--- client/modules/user-application/user-application.component.ts
+++ client/modules/user-application/user-application.component.ts
@@ -1,6 +1,8 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
 import {Component, ViewEncapsulation} from "@angular/core";
 import {MdIconRegistry} from '@angular2-material/icon';
+import {AuthService} from 'client/service/auth.service';
+import {User} from 'client/entities/entities';
 
 @Component({
     selector: 'user-application',
@@ -9,5 +11,16 @@ import {MdIconRegistry} from '@angular2-material/icon';
     viewProviders: [MdIconRegistry],
     encapsulation: ViewEncapsulation.None
 })
-export class UserApplicationComponent {
+export class UserApplicationComponent implements OnInit {
+
+    constructor(protected AuthService: AuthService) {
+
+    }
+
+    ngOnInit() {
+        this.AuthService.getAuthUser()
+            .then(authUser => this.authUser = authUser);
+    }
+
+    authUser: User = null;
 }
