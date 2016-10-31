10ce1a641a7bfa423d67c65d7ee7164da9b36dae
antonvovchenko <antonvovchenko@cyberco.com>
2016-10-31 17:01:34 +0200

    Styles changes; styles for login page

--- client/modules/auth/auth.component.ts
+++ client/modules/auth/auth.component.ts
@@ -2,10 +2,7 @@ import {Component} from "@angular/core";
 
 @Component({
     template: `
-    <div style=" width: 1160px; margin: 0 auto; margin-top: 10px;">
-        <h2>Authorization</h2>    
         <router-outlet></router-outlet>
-    </div>
   `
 })
 export class AuthComponent {
