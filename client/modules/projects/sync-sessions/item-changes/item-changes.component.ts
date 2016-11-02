378df5382eff813ab5d4ab7c79b5d6523508abce
mos <myownsumm@gmail.com>
2016-11-02 10:37:32 +0200

    base item changes component

--- client/modules/projects/sync-sessions/item-changes/item-changes.component.ts
+++ client/modules/projects/sync-sessions/item-changes/item-changes.component.ts
@@ -1,12 +1,11 @@
-import {Component, OnInit} from "@angular/core";
+import {Component, OnInit, Input} from "@angular/core";
 import {MsSyncClientService} from 'client/service/microservices/ms-sync-client.service';
 import {AuthService} from 'client/service/auth.service';
 import {Router, ActivatedRoute, Params} from '@angular/router';
-import {ItemChanges} from 'client/entities/entities';
+import {ItemChanges, SyncSession} from 'client/entities/entities';
 
 @Component({
-    // moduleId: module.id,
-    selector: "item-changes",
+    selector: 'item-changes',
     templateUrl: 'client/modules/projects/sync-sessions/item-changes/item-changes.component.html',
     styleUrls: ['client/modules/projects/sync-sessions/item-changes/item-changes.component.css']
 })
@@ -17,22 +16,16 @@ export class ItemChangesComponent implements OnInit {
                 private router: Router) {
     }
 
-    itemChangesList: [ItemChanges] = null;
+    protected itemChangesList: ItemChanges[] = null;
+
+    @Input('sync-session') syncSession: SyncSession;
 
     ngOnInit() {
-        this.route.params.forEach((params: Params) => {
-            let syncSessionId = +params['sync_session_id'];
+        this.MsSyncClientService.getItemChangesBySyncSessionsId(this.syncSession.id, this.AuthService.authUser.auth_session_id)
+            .then(itemChangesList => {
+                this.itemChangesList = itemChangesList;
 
-            this.MsSyncClientService.getItemChangesBySyncSessionsId(syncSessionId, this.AuthService.authUser.auth_session_id)
-                .then(itemChangesList => {
-                    this.itemChangesList = this.orderByDate(itemChangesList);
-                });
-        });
-    }
-
-    orderByDate(list: [{created_at}]) { // todo move this to some common pipe filter
-        return list.sort(function (b, a) {
-            return new Date(a.created_at).getTime() - new Date(b.created_at).getTime()
-        });
+                console.log('this.itemChangesList', this.itemChangesList);
+            });
     }
 }
