836a99b077e819585ed44970670d8d355b9c11ba
antonvovchenko <antonvovchenko@cyberco.com>
2016-11-02 14:04:56 +0200

    mark not implemented things

--- client/components/shared/usermenu.component.ts
+++ client/components/shared/usermenu.component.ts
@@ -17,11 +17,11 @@ import {User} from 'client/entities/entities';
     <li>
         <a href="javascript: void(0);" (click)="showUserMenu = !showUserMenu;">{{authUserEmail}} <i class="material-icons">arrow_drop_down</i></a>
         <ul *ngIf="showUserMenu" class="dropdown-menu" style="display: block;">
-            <li><a href="javascript: void(0);"  (click)="showUserMenu = !showUserMenu;">Company Settings</a></li>
+            <li><a href="javascript: void(0);"  (click)="showUserMenu = !showUserMenu;">**Company Settings</a></li>
             <li><a [routerLink]="['/connection']" (click)="showUserMenu = !showUserMenu;">Connections</a></li>
             <li><a [routerLink]="['/billing/licenses']" (click)="showUserMenu = !showUserMenu;">Billing Information</a></li>
             <li class="divider"></li>
-            <li><a href="javascript: void(0);"  (click)="showUserMenu = !showUserMenu;">Log out</a></li>
+            <li><a href="javascript: void(0);"  (click)="showUserMenu = !showUserMenu;">**Log out</a></li>
         </ul>
     </li>
 </ul>
