4b9f78d5255fd944d0a7408b8bc75b3da5e10217
mos <myownsumm@gmail.com>
2016-12-15 17:37:44 +0200

    useless libs removed

--- client/intefraces/wizard-component.interface.ts
+++
@@ -1,3 +0,0 @@
-export abstract class WizardComponentInterface {
-    abstract performAction();
-}
--- tools/systemjs.config.js
+++ tools/systemjs.config.js
@@ -25,8 +25,6 @@ var isPublic = typeof window != "undefined";
             '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
             // other libraries
             'rxjs': 'npm:rxjs',
-            'angular2-in-memory-web-api': 'npm:angular2-in-memory-web-api',
-            'angular2-jwt': 'npm:angular2-jwt/angular2-jwt.js',
             '@angular/material': 'npm:@angular/material'
         },
         // packages tells the System loader how to load when no filename and/or no extension
