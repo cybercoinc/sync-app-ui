fbe1f2826105442518c8a11f13e169a842212911
valeriy <valerchyck937@gmail.com>
2016-12-30 13:56:42 +0200

    "_status" to "_ah/health"

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ var express = require("express");
 var path_1 = require("path");
 var body_parser_1 = require("body-parser");
 var config = require('config');
+var cors = require('cors');
 var app = express();
 exports.app = app;
 app.disable("x-powered-by");
@@ -11,6 +12,18 @@ app.use(express.static(path_1.join(__dirname, '../public')));
 app.use(body_parser_1.json());
 app.use(body_parser_1.urlencoded({ extended: true }));
 app.use('/client', express.static(path_1.join(__dirname, '../client')));
+var originsWhitelist = [
+    'http://localhost:4500',
+    'https://sc-dot-app-status-page.appspot-preview.com',
+];
+var corsOptions = {
+    origin: function (origin, callback) {
+        var isWhitelisted = originsWhitelist.indexOf(origin) !== -1;
+        callback(null, isWhitelisted);
+    },
+    credentials: true
+};
+app.use(cors(corsOptions));
 // error handlers
 // development error handler
 // will print stacktrace
@@ -29,6 +42,15 @@ app.get('/configs', function (req, res, next) {
     return res.json({
         result: config
     });
+});
+app.get('/_ah/health', function (req, res, next) {
+    var response = {
+        status: "Ok",
+        message: "",
+        details: [],
+        config: config.util.getConfigSources()
+    };
+    return res.json(response);
 });
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
