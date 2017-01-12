fa50b114c2cb1eff084eb0d700729a2b9991000d
mos <myownsumm@gmail.com>
2017-01-12 17:32:48 +0200

    /me fix

--- client/routes.ts
+++ client/routes.ts
@@ -1,12 +1,8 @@
 import {Routes, RouterModule} from '@angular/router';
-import {AuthBootstrapService} from "client/service/resolvers/auth-bootstrap.service";
 
 export const routes: Routes = [
     {
         path: '',
-        resolve: {
-            bootstrap: AuthBootstrapService,
-        },
         redirectTo: 'projects',
         pathMatch: 'full'
     },
