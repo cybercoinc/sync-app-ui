f4910d79f554a8cce505c2e1d7f6b3c7e012682d
anastasiia <anastasiia.kharchenko@cyberco.com>
2017-01-18 18:40:09 +0200

    #1191 add viewing company name

--- client/components/shared/usermenu.component.ts
+++ client/components/shared/usermenu.component.ts
@@ -1,6 +1,5 @@
 import {Component, ElementRef, Input} from "@angular/core";
-import {AuthService} from 'client/service/auth.service';
-import {User} from 'client/entities/entities';
+import {User, Company} from 'client/entities/entities';
 import {MsUserClientService} from "client/service/microservices/ms-user-client.service";
 
 @Component({
@@ -11,16 +10,22 @@ import {MsUserClientService} from "client/service/microservices/ms-user-client.s
     styles: [
         '.dropdown-menu li a {color:#EBEBEB}',
         '.dropdown-menu li a:hover, .dropdown-menu li a:active {color:#666666}',
-        'ul {background-color: #44556b;}'
+        'ul {background-color: #44556b;}',
+        '.company-name { display: block; text-align: right; padding-right: 10%;}'
     ],
     template: `
 <ul class="nav navbar-nav navbar-right" *ngIf="authUser && authUser.role !== 'guest'">
     <li>
-        <a href="javascript: void(0);" (click)="showUserMenu = !showUserMenu;">{{authUser?.email}} <i class="material-icons">arrow_drop_down</i></a>
+        <a href="javascript: void(0);" (click)="showUserMenu = !showUserMenu;">
+            {{authUser?.email}}
+            <i class="material-icons">arrow_drop_down</i>    
+        </a>
+        <span class="company-name">{{company.name}}</span> 
         <ul *ngIf="showUserMenu" class="dropdown-menu" style="display: block;">
             <li><a [routerLink]="['/companies/settings']"  (click)="showUserMenu = !showUserMenu;">Company Settings</a></li>
             <li><a [routerLink]="['/connection']" (click)="showUserMenu = !showUserMenu;">Connections</a></li>
             <li><a [routerLink]="['/billing/licenses']" (click)="showUserMenu = !showUserMenu;">Billing Information</a></li>
+            <li><a href="javascript: void(0);"  (click)="switchCompany()">Switch Company</a></li>
             <li class="divider"></li>
             <li><a href="javascript: void(0);"  (click)="logout()">Log out</a></li>
         </ul>
@@ -29,12 +34,12 @@ import {MsUserClientService} from "client/service/microservices/ms-user-client.s
 `,
 })
 export class UserMenuComponent {
-
-    constructor(private _eref: ElementRef, protected AuthService: AuthService, protected MsUserClientService: MsUserClientService) {
+    constructor(private _eref: ElementRef, protected MsUserClientService: MsUserClientService) {
     }
 
     showUserMenu: boolean = false;
     @Input('user') authUser: User;
+    @Input('company') company: Company;
 
     onClick(event) {
         event.stopPropagation();
@@ -47,5 +52,9 @@ export class UserMenuComponent {
     logout() {
         return this.MsUserClientService.logout()
             .then(response => window.location.replace('/'));
+    }
+
+    switchCompany() {
+
     }
 }
