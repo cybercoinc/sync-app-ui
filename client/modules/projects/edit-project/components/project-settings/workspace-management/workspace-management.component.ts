25929fdd00e497350068c5e30dc6439e828dae7a
Alexander Poltoratskiy <myownsumm@gmail.com>
2017-02-17 13:17:49 +0200

    check if user allowed to disconnect pipe

--- client/modules/projects/edit-project/components/project-settings/workspace-management/workspace-management.component.ts
+++ client/modules/projects/edit-project/components/project-settings/workspace-management/workspace-management.component.ts
@@ -2,7 +2,7 @@ import {Component, Input, OnInit} from "@angular/core";
 import {AuthService} from "client/service/auth.service";
 import {ActivatedRoute} from "@angular/router";
 import {MsProjectClientService} from "client/service/microservices/ms-project-client.service";
-import {SmartsheetWorkspace} from "client/entities/entities";
+import {Project, SmartsheetWorkspace} from "client/entities/entities";
 
 @Component({
     selector: 'workspace-management',
@@ -18,20 +18,31 @@ export class WorkspaceManagementComponent implements OnInit {
     }
 
     ngOnInit() {
-        this.msProjectClientService.getSmartsheetWorkspace(this.projectId)
-            .then(workspace => {
-                this.workspace = workspace;
+        Promise.all([
+            this.msProjectClientService.getSmartsheetWorkspace(this.projectId),
+            this.msProjectClientService.getPipesWhere({
+                project_fk_id: this.projectId
+            })
+        ])
+            .then(results => {
+                this.workspace = results[0];
+                this.canDisconnectWorkspace = results[1].length === 0;
             });
     }
 
     @Input('project-id') projectId: number;
 
     protected workspace: SmartsheetWorkspace;
+    protected project: Project;
 
     disconnectWorkspace() {
+        this.canDisconnectWorkspace = false;
+
         return this.msProjectClientService.disconnectWorkspace(this.projectId)
             .then(() => {
                 this.workspace = null;
             })
     }
+
+    canDisconnectWorkspace: boolean = false;
 }
