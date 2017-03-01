0bc5753779187678c5d87ce8469a81dc782297a9
anastasiia <anastasiia.kharchenko@cyberco.com>
2017-03-01 12:51:08 +0200

    #460 change design and add setting

--- client/modules/projects/edit-project/components/shared/pipe-settings/pipe-settings.component.ts
+++ client/modules/projects/edit-project/components/shared/pipe-settings/pipe-settings.component.ts
@@ -25,9 +25,7 @@ export class PipeSettingsComponent implements OnInit {
             let pipeObj = this.pipesListObj[this.pipeType];
 
             let propsToSet = [
-                'sm_working_days',
-                'summary_tasks_enabled',
-                'colors_coding_enabled'
+                'summary_tasks_enabled'
             ];
 
             propsToSet.forEach(propName => {
@@ -36,9 +34,6 @@ export class PipeSettingsComponent implements OnInit {
                 }
             });
 
-            if (pipeObj.sm_weekends) {
-                this.model.sm_weekends = pipeObj.sm_weekends.join(',');
-            }
         }
     }
 
@@ -46,29 +41,15 @@ export class PipeSettingsComponent implements OnInit {
 
     protected pipesListObj;
 
-    public model: {sm_working_days: {}, sm_weekends: string, summary_tasks_enabled: boolean, colors_coding_enabled: boolean} = {
-        sm_working_days: {
-            mon: true,
-            tue: true,
-            wed: true,
-            thu: true,
-            fri: true,
-            sat: false,
-            sun: false
-        },
-        sm_weekends: '',
-        summary_tasks_enabled: false,
-        colors_coding_enabled: true,
+    public model: {summary_tasks_enabled: boolean} = {
+        summary_tasks_enabled: false
     };
 
     saveAndContinue() {
         return this.PipeConnectionService.createNewOrGetExistingPipe(this.pipeType)
             .then(pipeId => {
                 return this.MsProjectClientService.updatePipe(pipeId, {
-                    sm_working_days: this.model.sm_working_days,
-                    sm_weekends: this.model.sm_weekends.split(','),
-                    summary_tasks_enabled: this.model.summary_tasks_enabled,
-                    colors_coding_enabled: this.model.colors_coding_enabled
+                    summary_tasks_enabled: this.model.summary_tasks_enabled
                 });
             })
             .then(() => {
