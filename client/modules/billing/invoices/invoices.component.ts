01905531f8cde8d49a47ce5fd13d8b4c513348cb
valeriy <valerchyck937@gmail.com>
2017-04-06 13:54:17 +0300

    print invoice in PDF format

--- client/modules/billing/invoices/invoices.component.ts
+++ client/modules/billing/invoices/invoices.component.ts
@@ -2,6 +2,7 @@ import {Component} from "@angular/core";
 import {AuthService} from 'client/service/auth.service';
 import {MsLicenseClientService} from 'client/service/microservices/ms-license-client.service';
 import {Invoice} from "client/entities/entities";
+import {ConfigService} from "client/service/config.service";
 
 @Component({
     selector: "billingInvoices",
@@ -10,8 +11,13 @@ import {Invoice} from "client/entities/entities";
 })
 export class InvoicesComponent {
     protected invoices: Invoice[];
+    protected printPDFUrl: string;
 
-    constructor(protected AuthService: AuthService, protected MsLicenseClientService: MsLicenseClientService) {
+    constructor(protected AuthService: AuthService,
+                protected MsLicenseClientService: MsLicenseClientService,
+                protected ConfigService: ConfigService) {
+        this.printPDFUrl = this.ConfigService.getServiceUrl(this.MsLicenseClientService.msName) + 'print-invoice-pdf?invoice_id=';
+
         MsLicenseClientService.getInvoices(this.AuthService.authUser.id, this.AuthService.company.id)
             .then(invoices => {
                 this.invoices = invoices;
