2882f746d687634117bb59e45412992970d6b137
valeriy <valerchyck937@gmail.com>
2017-04-10 18:05:00 +0300

    cron for policy notifications

--- client/modules/projects/edit-project/components/project-settings/task-notifications/policy/policy.component.ts
+++ client/modules/projects/edit-project/components/project-settings/task-notifications/policy/policy.component.ts
@@ -33,9 +33,9 @@ export class PolicyComponent implements OnInit {
         'saturday',
         'sunday',
         'separator',
-        'every 1st day of month',
-        'every 15th day of month',
-        'every last day of month',
+        'every_1st_day_of_month',
+        'every_15th_day_of_month',
+        'every_last_day_of_month',
     ];
     private times = [
         '01:00 am', '02:00 am', '03:00 am', '04:00 am', '05:00 am', '06:00 am', '07:00 am', '08:00 am', '09:00 am', '10:00 am', '11:00 am', '12:00 am',
