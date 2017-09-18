06682f672ce8f7c883d6bec15efae4745f7a8a1c
antonvovchenko <anton.vovchenko@cyberco.com>
2017-09-18 15:41:52 +0300

    forse https for app-ui

--- server/app.ts
+++ server/app.ts
@@ -4,10 +4,17 @@ import {join} from "path";
 import {json, urlencoded} from "body-parser";
 let config = require('config');
 let cors = require('cors');
+const yes = require('yes-https');
 
 
 const app: express.Application = express();
 app.disable("x-powered-by");
+
+app.use(yes({
+    ignoreFilter: (req) => {
+        return (req.url.indexOf('/_ah/health') > -1);
+    }
+}));
 
 app.use(express.static(join(__dirname, '../public')));
 app.use(json());
