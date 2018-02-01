76926b09c73a848c0665450bdfaa1437e83bad55
alexander.poltoratskiy <myownsumm@gmail.com>
2018-02-01 19:22:21 +0200

    edit document pipe OK

--- client/modules/projects/edit-project/components/document-pipes/edit-document-pipe/edit-document-pipe.component.ts
+++ client/modules/projects/edit-project/components/document-pipes/edit-document-pipe/edit-document-pipe.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Route } from '@angular/router';
+import { MsProjectClientService } from '../../../../../../service/microservices/ms-project-client.service';
+import { ProjectPipe } from '../../../../../../entities/entities';
 
 
 @Component({
@@ -12,12 +14,17 @@ import { ActivatedRoute, Route } from '@angular/router';
 export class EditDocumentPipeComponent implements OnInit {
     protected pipeId: number;
 
-    ngOnInit() {
+    protected pipe: ProjectPipe;
 
-        console.log(this.pipeId);
+    ngOnInit() {
+        return this.msProjectService.getPipeById(this.pipeId)
+            .then(pipes => {
+                this.pipe = pipes.shift();
+            })
     }
 
-    constructor(protected activatedRoute: ActivatedRoute) {
+    constructor(protected activatedRoute: ActivatedRoute,
+                protected msProjectService: MsProjectClientService) {
         this.activatedRoute.params.subscribe(params => {
             this.pipeId = +params['pipe_id'];
         });
